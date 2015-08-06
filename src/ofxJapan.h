62ddf6db1fc448af54de49be430287ae2def4d0b
Motoi Shimizu <shimizu.motoi@gmail.com>
2015-08-06 13:16:02 +0900

    initial & final commit

--- src/ofxJapan.h
+++ src/ofxJapan.h
@@ -5,8 +5,9 @@
  http://www.wtfpl.net/
  
  Reading materials
- https://twitter.com/Kezzardrix/status/629106664814219264
- https://www.google.co.jp/search?espv=2&biw=1516&bih=1055&tbm=isch&sa=1&q=tokyo+2020+emblem+ripoff&oq=tokyo+2020+emblem+ripoff&gs_l=img.3...2712.4568.0.4642.7.7.0.0.0.0.94.461.7.7.0....0...1c.1.64.img..7.0.0.JBPmBZi2pyQ
+ - http://kyoko-np.net/2015080601.html
+ - https://twitter.com/Kezzardrix/status/629106664814219264
+ - Google image search: [Tokyo 2020 emblem ripoff](https://www.google.co.jp/search?espv=2&biw=1516&bih=1055&tbm=isch&sa=1&q=tokyo+2020+emblem+ripoff&oq=tokyo+2020+emblem+ripoff&gs_l=img.3...2712.4568.0.4642.7.7.0.0.0.0.94.461.7.7.0....0...1c.1.64.img..7.0.0.JBPmBZi2pyQ)
  */
 
 class ofxJapan {
